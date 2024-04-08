322ff572cbe6e985944a97f40af1f852a3136068
Blackcyan30 <talhaadnan2003@gmail.com>
2024-04-08 01:32:05 -0500

    Initial Commit

---
+++ Project3Client/src/main/java/GuiClient.java
@@ -0,0 +1,79 @@
+
+import java.util.HashMap;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class GuiClient extends Application{
+
+	
+	TextField c1;
+	Button b1;
+	HashMap<String, Scene> sceneMap;
+	VBox clientBox;
+	Client clientConnection;
+	
+	ListView<String> listItems2;
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		clientConnection = new Client(data->{
+				Platform.runLater(()->{listItems2.getItems().add(data.toString());
+			});
+		});
+							
+		clientConnection.start();
+
+		listItems2 = new ListView<String>();
+		
+		c1 = new TextField();
+		b1 = new Button("Send");
+		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
+		
+		sceneMap = new HashMap<String, Scene>();
+
+		sceneMap.put("client",  createClientGui());
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+
+		primaryStage.setScene(sceneMap.get("client"));
+		primaryStage.setTitle("Client");
+		primaryStage.show();
+		
+	}
+	
+
+	
+	public Scene createClientGui() {
+		
+		clientBox = new VBox(10, c1,b1,listItems2);
+		clientBox.setStyle("-fx-background-color: blue;"+"-fx-font-family: 'serif';");
+		return new Scene(clientBox, 400, 300);
+		
+	}
+
+}
