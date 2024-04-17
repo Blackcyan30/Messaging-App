5a804bf391de29dd52179a1612d3019ff456b838
Blackcyan30 <talhaadnan2003@gmail.com>
2024-04-16 21:35:11 -0500

    Updated pom and partially completed server

--- Project3Server/src/main/java/GuiServer.java
+++ Project3Server/src/main/java/GuiServer.java
@@ -1,74 +1,74 @@
 
-import java.util.HashMap;
+// import java.util.HashMap;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+// import javafx.application.Application;
+// import javafx.application.Platform;
+// import javafx.event.EventHandler;
+// import javafx.geometry.Insets;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.ListView;
+// import javafx.scene.control.TextField;
+// import javafx.scene.layout.BorderPane;
+// import javafx.scene.layout.GridPane;
+// import javafx.scene.layout.HBox;
+// import javafx.scene.layout.VBox;
+// import javafx.stage.Stage;
+// import javafx.stage.WindowEvent;
 
-public class GuiServer extends Application{
+// public class GuiServer extends Application{
 
-	HashMap<String, Scene> sceneMap;
-	Server serverConnection;
+// 	HashMap<String, Scene> sceneMap;
+// 	Server serverConnection;
 	
-	ListView<String> listItems, listItems2;
+// 	ListView<String> listItems, listItems2;
 	
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+// 	public static void main(String[] args) {
+// 		launch(args);
+// 	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		serverConnection = new Server(data -> {
-			Platform.runLater(()->{
-				listItems.getItems().add(data.toString());
-			});
-		});
+// 	@Override
+// 	public void start(Stage primaryStage) throws Exception {
+// 		serverConnection = new Server(data -> {
+// 			Platform.runLater(()->{
+// 				listItems.getItems().add(data.toString());
+// 			});
+// 		});
+		
+		
+// 		listItems = new ListView<String>();
 
+// 		sceneMap = new HashMap<String, Scene>();
 		
-		listItems = new ListView<String>();
+// 		sceneMap.put("server",  createServerGui());
+		
+// 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//             @Override
+//             public void handle(WindowEvent t) {
+//                 Platform.exit();
+//                 System.exit(0);
+//             }
+//         });
 
-		sceneMap = new HashMap<String, Scene>();
+// 		primaryStage.setScene(sceneMap.get("server"));
+// 		primaryStage.setTitle("This is the Server");
+// 		primaryStage.show();
 		
-		sceneMap.put("server",  createServerGui());
-		
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
-
-		primaryStage.setScene(sceneMap.get("server"));
-		primaryStage.setTitle("This is the Server");
-		primaryStage.show();
-		
-	}
+// 	}
 	
-	public Scene createServerGui() {
+// 	public Scene createServerGui() {
 		
-		BorderPane pane = new BorderPane();
-		pane.setPadding(new Insets(70));
-		pane.setStyle("-fx-background-color: coral");
+// 		BorderPane pane = new BorderPane();
+// 		pane.setPadding(new Insets(70));
+// 		pane.setStyle("-fx-background-color: coral");
 		
-		pane.setCenter(listItems);
-		pane.setStyle("-fx-font-family: 'serif'");
-		return new Scene(pane, 500, 400);
+// 		pane.setCenter(listItems);
+// 		pane.setStyle("-fx-font-family: 'serif'");
+// 		return new Scene(pane, 500, 400);
 		
 		
-	}
+// 	}
 
 
-}
+// }
--- Project3Server/src/main/java/Server.java
+++ Project3Server/src/main/java/Server.java
@@ -8,111 +8,246 @@ import java.util.function.Consumer;
 
 import javafx.application.Platform;
 import javafx.scene.control.ListView;
-/*
- * Clicker: A: I really get it    B: No idea what you are talking about
- * C: kind of following
- */
+
+
+// public class Server{
+	
+// 	// Initialized to 1 to indicte first client conncetion.
+// 	int count = 1;
+// 	// Change name to clientsList to be more readable.
+// 	ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
+// 	// Change name to be less bad.
+// 	TheServer server;
+// 	// Don't fully understand this need to learn
+// 	private Consumer<Serializable> callback;
+	
+	
+// 	Server(Consumer<Serializable> call){
+	
+// 		callback = call;
+// 		server = new TheServer();
+// 		server.start();
+// 	}
+	
+// 	class TheServer extends Thread {
+		
+// 		public void run() {
+		
+// 			try(ServerSocket mysocket = new ServerSocket(5555);) {
+
+// 		    System.out.println("Server is waiting for a client!");
+		  
+			
+// 		    while(true) {
+		
+// 				ClientThread clientThread = new ClientThread(mysocket.accept(), count);
+// 				callback.accept("client has connected to server: " + "client #" + count);
+// 				clients.add(clientThread);
+// 				clientThread.start();
+				
+// 				count++;
+// 			    }
+// 			} catch(Exception e) {
+// 					callback.accept("Server socket did not launch");
+// 				}
+// 			}
+// 		}
+	
+
+// 		class ClientThread extends Thread{
+			
+// 			String userName;
+// 			Socket connection;
+// 			int count;
+// 			ObjectInputStream inputStream;
+// 			ObjectOutputStream outputStream;
+			
+// 			ClientThread(Socket s, int count){
+// 				this.connection = s;
+// 				this.count = count;	
+// 			}
+			
+// 			/**
+// 			 * Method that sends message to all clients.
+// 			 */
+// 			public void updateClients(String message) {
+// 				for(int i = 0; i < clients.size(); i++) {
+// 					ClientThread clientThread = clients.get(i);
+// 					try {
+// 					 clientThread.outputStream.writeObject(message);
+// 					}
+// 					catch(Exception e) {
+// 						e.printStackTrace();
+// 					}
+// 				}
+// 			}
+			
+// 			public void run(){
+					
+// 				try {
+// 					inputStream = new ObjectInputStream(connection.getInputStream());
+// 					outputStream = new ObjectOutputStream(connection.getOutputStream());
+// 					connection.setTcpNoDelay(true);
+// 				}
+// 				catch(Exception e) {
+// 					System.out.println("Streams not open");
+// 				}
+				
+// 				updateClients("new client on server: client #"+count);
+					
+// 				 while(true) {
+// 					    try {
+// 					    	String data = inputStream.readObject().toString();
+// 					    	callback.accept("client: " + count + " sent: " + data);
+// 					    	updateClients("client #" + count + " said: " + data);
+					    	
+// 					    	}
+// 					    catch(Exception e) {
+// 					    	callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
+// 					    	updateClients("Client #"+count+" has left the server!");
+// 					    	clients.remove(this);
+// 					    	break;
+// 					    }
+// 					}
+// 				}//end of run
+			
+			
+// 		}//end of client thread
+// }
+
+
+
+
+
+
+
+
+
+
+// {arguments} -> {Statements to be executed.}
 
 public class Server{
-
-	int count = 1;	
-	ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
+	
+	// Initialized to 1 to indicte first client conncetion.
+	int count = 1;
+	// Change name to clientsList to be more readable.
+	ArrayList<ClientThread> clientsList = new ArrayList<ClientThread>();
+	// Change name to be less bad.
 	TheServer server;
-	private Consumer<Serializable> callback;
+	// Don't fully understand this need to learn
+	// private Consumer<Serializable> callback;
 	
 	
-	Server(Consumer<Serializable> call){
+	// Server(Consumer<Serializable> call){
 	
-		callback = call;
-		server = new TheServer();
-		server.start();
-	}
+	// 	callback = call;
+	// 	server = new TheServer();
+	// 	server.start();
+	// }
 	
-	
-	public class TheServer extends Thread{
+	class TheServer extends Thread {
 		
 		public void run() {
 		
-			try(ServerSocket mysocket = new ServerSocket(5555);){
-		    System.out.println("Server is waiting for a client!");
-		  
-			
-		    while(true) {
-		
-				ClientThread c = new ClientThread(mysocket.accept(), count);
-				callback.accept("client has connected to server: " + "client #" + count);
-				clients.add(c);
-				c.start();
-				
-				count++;
-				
-			    }
-			}//end of try
-				catch(Exception e) {
-					callback.accept("Server socket did not launch");
-				}
-			}//end of while
-		}
-	
+			try(ServerSocket mysocket = new ServerSocket(5555);) {
 
+				System.out.println("Server is waiting for a client!");
+			
+				while(true) {
+			
+					ClientThread client = new ClientThread(mysocket.accept(), count);
+					// callback.accept("client has connected to server: " + "client #" + count);
+					System.out.println("Client connnected with name: " + client.userName + " with the port: " + client.getSocket().getPort() + ".");
+					clientsList.add(client);
+					client.start();
+					count++;
+				}
+			} catch(Exception e) {
+					e.printStackTrace();
+			}
+		}
+	}
+	
 		class ClientThread extends Thread{
 			
-		
+			String userName;
 			Socket connection;
 			int count;
-			ObjectInputStream in;
-			ObjectOutputStream out;
-			
-			ClientThread(Socket s, int count){
-				this.connection = s;
-				this.count = count;	
+			ObjectInputStream inputStream;
+			ObjectOutputStream outputStream;
+
+			/**
+			 * Gets client socket connection
+			 * @return Socket on which client connected.
+			 */
+			public Socket getSocket() {
+				return connection;
 			}
 			
-			public void updateClients(String message) {
-				for(int i = 0; i < clients.size(); i++) {
-					ClientThread t = clients.get(i);
-					try {
-					 t.out.writeObject(message);
-					}
-					catch(Exception e) {}
+			/**
+			 * Constructor for the clientThread. Takes in a socket for a unique connection.
+			 * @param socket
+			 * @param count
+			 */
+			ClientThread(Socket socket, int count){
+				this.connection = socket;
+				this.count = count;
+			}
+			
+			/**
+			 * Method that sends message to all clients.
+			 */
+			public void updateClients(String message) throws Exception {
+				for(int i = 0; i < clientsList.size(); i++) {
+					ClientThread clientThread = clientsList.get(i);
+					clientThread.outputStream.writeObject(message);
+				}
+			}
+
+			public void readIncomingMessage(Message message) {
+				if (message.actionCode == 0) {
+
+				} else if (message.actionCode == 1) {
+
+				} else {
+					
 				}
 			}
 			
 			public void run(){
-					
+
 				try {
-					in = new ObjectInputStream(connection.getInputStream());
-					out = new ObjectOutputStream(connection.getOutputStream());
-					connection.setTcpNoDelay(true);	
+					inputStream = new ObjectInputStream(connection.getInputStream());
+					outputStream = new ObjectOutputStream(connection.getOutputStream());
+					connection.setTcpNoDelay(true);
 				}
 				catch(Exception e) {
 					System.out.println("Streams not open");
 				}
-				
-				updateClients("new client on server: client #"+count);
-					
-				 while(true) {
+				try{
+					updateClients("new client on server: client #"+count);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				while(true) {
 					    try {
-					    	String data = in.readObject().toString();
-					    	callback.accept("client: " + count + " sent: " + data);
-					    	updateClients("client #"+count+" said: "+data);
+					    	Message data = (Message)inputStream.readObject();
+
+					    	updateClients("client #" + count + " said: " + data);
 					    	
 					    	}
 					    catch(Exception e) {
-					    	callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
-					    	updateClients("Client #"+count+" has left the server!");
-					    	clients.remove(this);
+							try {
+								updateClients("Client #"+count+" has left the server!");
+							} catch (Exception excpt) {
+								excpt.printStackTrace();
+							}
+					    	clientsList.remove(this);
 					    	break;
 					    }
 					}
-				}//end of run
+				}
 			
 			
 		}//end of client thread
 }
-
-
-	
-	
-
-	
